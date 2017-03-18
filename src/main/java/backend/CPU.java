40066f34e40c6c9f65ec13d5f22469ca00f96656
PauliusMr <mrpaulius@yahoo.com>
2017-03-18 19:29:50 +0200

    made JA and JE

--- src/main/java/backend/CPU.java
+++ src/main/java/backend/CPU.java
@@ -9,10 +9,10 @@ import java.util.Arrays;
  *
  */
 public class CPU {
-	
+
 	private MemoryUnit ramClass;
 	private Byte[][] ram;
-	
+
 	public Byte[] IC = { new Byte((byte) 0), new Byte((byte) 0) };
 	public Byte[] SP = { new Byte((byte) 0), new Byte((byte) 0) };
 
@@ -33,24 +33,22 @@ public class CPU {
 		this.ram = ram.memory;
 	}
 
-	
-	//Place for methods describing instructions
-	
-	
-	public void ADD(){
+	// Place for methods describing instructions
+
+	public void ADD() {
 		Byte[] SPtmp = iterateRegister(SP, -3);
 		byte a = ram[SPtmp[1]][SPtmp[2]];
 		SPtmp = iterateRegister(SP, -2);
 		byte b = ram[SPtmp[1]][SPtmp[2]];
 		SPtmp = iterateRegister(SP, -1);
-		byte c = ram[SPtmp[1]][SPtmp[2]];		
-		byte d = ram[SP[1]][SP[2]]; //SP nes imam be poslinkio
-		short val1=(short)( ((a)<<8) | (b) );
-		short val2=(short)( ((c)<<8) | (d) );
-		short sum = (short)(val1+val2);
+		byte c = ram[SPtmp[1]][SPtmp[2]];
+		byte d = ram[SP[1]][SP[2]]; // SP nes imam be poslinkio
+		short val1 = (short) (((a) << 8) | (b));
+		short val2 = (short) (((c) << 8) | (d));
+		short sum = (short) (val1 + val2);
 		byte a1 = (byte) sum;
 		byte a2 = (byte) (sum >> 8);
-		
+
 		SPtmp = iterateRegister(SP, -2);
 		ram[SPtmp[1]][SPtmp[2]] = a1;
 		SPtmp = iterateRegister(SP, -1);
@@ -59,26 +57,24 @@ public class CPU {
 		SPtmp = iterateRegister(SP, -2);
 		SP = SPtmp;
 		decrementTimer();
-		
-		SPtmp = iterateRegister(SP, -2);
-		SP = SPtmp;		
-	}
-	
 
+		SPtmp = iterateRegister(SP, -2);
+		SP = SPtmp;
+	}
 
 	private Byte[] iterateRegister(Byte[] reg, int stepsAmount) {
 		Byte[] naujas = { reg[0], reg[1] };
 		int stpAm = Math.abs(stepsAmount);
 		for (int i = 0; i < stpAm; i++) {
 			if (stepsAmount > 0) {
-				if (++naujas[1] == 0x00){
-					if(++naujas[0] == 16 && MDR == 0){
+				if (++naujas[1] == 0x00) {
+					if (++naujas[0] == 16 && MDR == 0) {
 						naujas[0] = 0;
 					}
 				}
 			} else {
-				if (--naujas[1] == 0xFF){
-					if(--naujas[0] == 0xFF && MDR == 0){
+				if (--naujas[1] == 0xFF) {
+					if (--naujas[0] == 0xFF && MDR == 0) {
 						naujas[0] = 15;
 					}
 				}
@@ -86,11 +82,11 @@ public class CPU {
 		}
 		return naujas;
 	}
-	
-	private void decrementTimer(){
-		if(TI>0){
+
+	private void decrementTimer() {
+		if (TI > 0) {
 			TI--;
-		}		
+		}
 	}
 
 	public void SUB() {
@@ -99,67 +95,67 @@ public class CPU {
 		SPtmp = iterateRegister(SP, -2);
 		byte b = ram[SPtmp[1]][SPtmp[2]];
 		SPtmp = iterateRegister(SP, -1);
-		byte c = ram[SPtmp[1]][SPtmp[2]];		
-		byte d = ram[SP[1]][SP[2]]; //SP nes imam be poslinkio
-		short val1=(short)( ((a)<<8) | (b) );
-		short val2=(short)( ((c)<<8) | (d) );
-		short sum = (short)(val1-val2);
+		byte c = ram[SPtmp[1]][SPtmp[2]];
+		byte d = ram[SP[1]][SP[2]]; // SP nes imam be poslinkio
+		short val1 = (short) (((a) << 8) | (b));
+		short val2 = (short) (((c) << 8) | (d));
+		short sum = (short) (val1 - val2);
 		byte a1 = (byte) sum;
 		byte a2 = (byte) (sum >> 8);
-		
+
 		SPtmp = iterateRegister(SP, -2);
 		ram[SPtmp[1]][SPtmp[2]] = a1;
 		SPtmp = iterateRegister(SP, -1);
 		ram[SPtmp[1]][SPtmp[2]] = a2;
-		
+
 		SPtmp = iterateRegister(SP, -2);
 		SP = SPtmp;
 		decrementTimer();
 	}
-	
-	public void MUL(){
+
+	public void MUL() {
 		Byte[] SPtmp = iterateRegister(SP, -3);
 		byte a = ram[SPtmp[1]][SPtmp[2]];
 		SPtmp = iterateRegister(SP, -2);
 		byte b = ram[SPtmp[1]][SPtmp[2]];
 		SPtmp = iterateRegister(SP, -1);
-		byte c = ram[SPtmp[1]][SPtmp[2]];		
-		byte d = ram[SP[1]][SP[2]]; //SP nes imam be poslinkio
-		short val1=(short)( ((a)<<8) | (b) );
-		short val2=(short)( ((c)<<8) | (d) );
-		short sum = (short)(val1*val2);
+		byte c = ram[SPtmp[1]][SPtmp[2]];
+		byte d = ram[SP[1]][SP[2]]; // SP nes imam be poslinkio
+		short val1 = (short) (((a) << 8) | (b));
+		short val2 = (short) (((c) << 8) | (d));
+		short sum = (short) (val1 * val2);
 		byte a1 = (byte) sum;
 		byte a2 = (byte) (sum >> 8);
-		
+
 		SPtmp = iterateRegister(SP, -2);
 		ram[SPtmp[1]][SPtmp[2]] = a1;
 		SPtmp = iterateRegister(SP, -1);
 		ram[SPtmp[1]][SPtmp[2]] = a2;
-		
+
 		SPtmp = iterateRegister(SP, -2);
 		SP = SPtmp;
 		decrementTimer();
 	}
-	
-	public void DIV(){
+
+	public void DIV() {
 		Byte[] SPtmp = iterateRegister(SP, -3);
 		byte a = ram[SPtmp[1]][SPtmp[2]];
 		SPtmp = iterateRegister(SP, -2);
 		byte b = ram[SPtmp[1]][SPtmp[2]];
 		SPtmp = iterateRegister(SP, -1);
-		byte c = ram[SPtmp[1]][SPtmp[2]];		
-		byte d = ram[SP[1]][SP[2]]; //SP nes imam be poslinkio
-		short val1=(short)( ((a)<<8) | (b) );
-		short val2=(short)( ((c)<<8) | (d) );
-		short sum = (short)(val1/val2);
+		byte c = ram[SPtmp[1]][SPtmp[2]];
+		byte d = ram[SP[1]][SP[2]]; // SP nes imam be poslinkio
+		short val1 = (short) (((a) << 8) | (b));
+		short val2 = (short) (((c) << 8) | (d));
+		short sum = (short) (val1 / val2);
 		byte a1 = (byte) sum;
 		byte a2 = (byte) (sum >> 8);
-		
+
 		SPtmp = iterateRegister(SP, -2);
 		ram[SPtmp[1]][SPtmp[2]] = a1;
 		SPtmp = iterateRegister(SP, -1);
 		ram[SPtmp[1]][SPtmp[2]] = a2;
-		
+
 		SPtmp = iterateRegister(SP, -2);
 		SP = SPtmp;
 		decrementTimer();
@@ -173,7 +169,7 @@ public class CPU {
 		SPtmp = iterateRegister(SP, -1);
 		byte c = ram[SPtmp[1]][SPtmp[2]];
 		byte d = ram[SP[1]][SP[2]]; // SP nes imam be
-														// poslinkio
+									// poslinkio
 		short val1 = (short) (((a) << 8) | (b));
 		short val2 = (short) (((c) << 8) | (d));
 		byte rez;
@@ -191,4 +187,26 @@ public class CPU {
 		decrementTimer();
 	}
 
+	public void JPxy() {
+		Byte[] ICtmp = iterateRegister(IC, 1);
+		byte a = ram[ICtmp[1]][ICtmp[2]];
+		ICtmp = iterateRegister(IC, 2);
+		byte b = ram[IC[1]][IC[2]];
+		IC[0] = a;
+		IC[1] = b;
+		decrementTimer();
+	}
+
+	public void JExy() {
+		byte tmp = ram[SP[1]][SP[2]];
+		if (tmp == 1){
+			Byte[] ICtmp = iterateRegister(IC, 1);
+			byte a = ram[ICtmp[1]][ICtmp[2]];
+			ICtmp = iterateRegister(IC, 2);
+			byte b = ram[IC[1]][IC[2]];
+			IC[0] = a;
+			IC[1] = b;
+			SP = iterateRegister(SP, -1);
+		}
+	}
 }
